3a93d5ed035084e2bc1740ed348f5172bd0e793d
DaryaSHCH <67901229+DaryaSHCH@users.noreply.github.com>
2020-10-09 17:35:15 +0300

    Update TaskFourth.java
    Figuring out task without using methods of class Math

--- jonline-module01/src/com/jonline/module01/lineal/task04/TaskFourth.java
+++ jonline-module01/src/com/jonline/module01/lineal/task04/TaskFourth.java
@@ -3,18 +3,18 @@ package com.jonline.module01.lineal.task04;
  * swap the fractional and integer parts of a number;
  */
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 public class TaskFourth {
 	public static void main(String[] args) {
-        
-		double x = 567.128;
-        
-        double y; 
-        y = (int) x;
-        y = (x * 1000) % 1000 +(int) x / 1000.0;
-        System.out.println(y);
-        
-    }
+
+		double x;
+		x = 567.128;
+
+		double y;
+		y = (int) x;
+
+		y = (x * 1000) % 1000 + (int) x / 1000.0;
+
+		System.out.println(y);
+
+	}
 }
