d5ce9312abe9d5bfbe0e6f188391332617a7d140
DaryaSHCH <67901229+DaryaSHCH@users.noreply.github.com>
2020-10-12 21:48:04 +0300

    Update TaskEighth.java

--- jonline-module02/src/com/jonline/module02/multidimensional/task08/TaskEighth.java
+++ jonline-module02/src/com/jonline/module02/multidimensional/task08/TaskEighth.java
@@ -10,26 +10,33 @@ public class TaskEighth {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
+
 		int matrixLines;
+
 		System.out.println("Lines>>");
 
 		checkNumber(scan);
-		matrixLines= scan.nextInt();
 
-		while (matrixLines <= 0 ) {
+		matrixLines = scan.nextInt();
+
+		while (matrixLines <= 0) {
 			System.out.println("Input integer more then 0 >> ");
-			matrixLines= scan.nextInt();
+			matrixLines = scan.nextInt();
 		}
-		
+
 		int matrixColumns;
+
 		System.out.println("Columns>>");
+
 		checkNumber(scan);
+
 		matrixColumns = scan.nextInt();
+
 		while (matrixColumns <= 0) {
 			System.out.println("Input integer more then 0 >> ");
-			matrixColumns= scan.nextInt();
+			matrixColumns = scan.nextInt();
 		}
-		
+
 		int[][] matrix = new int[matrixLines][matrixColumns];
 
 		for (int i = 0; i < matrix.length; i++) {
@@ -49,13 +56,18 @@ public class TaskEighth {
 		}
 
 		System.out.println("Input number of column>>");
+
 		int firstColumn;
+
 		checkNumber(scan);
+
 		firstColumn = scan.nextInt();
+
 		while (firstColumn <= 0 && firstColumn > matrixColumns) {
 			System.out.println("Input positive number, within the matrix values>>");
 			firstColumn = scan.nextInt();
 		}
+
 		if (firstColumn == 1) {
 			firstColumn--;
 		} else {
@@ -63,8 +75,11 @@ public class TaskEighth {
 		}
 
 		System.out.println("Input the column number to swap with the previous column>>");
+
 		int nextColumn;
+
 		checkNumber(scan);
+
 		nextColumn = scan.nextInt();
 
 		while (nextColumn <= 0 && nextColumn > matrixColumns) {
@@ -77,23 +92,15 @@ public class TaskEighth {
 		} else {
 			nextColumn--;
 		}
-
-//		for (int i = 0; i < matrix.length; i++) {
-//			for (int j = 0; j < matrix[i].length; j++) {
-//				if (j == firstColumn) {
-//					matrix[i][j] = matrix[i][nextColumn];
-//				}
-//			}
-//		}
+		int rememberInt;
 
 		for (int i = 0; i < matrix.length; i++) {
-					
-			    int rememberInt = matrix[i][firstColumn];
-				matrix[i][firstColumn] = matrix[i][nextColumn];
-				matrix[i][nextColumn] = rememberInt;
+
+			rememberInt = matrix[i][firstColumn];
+			matrix[i][firstColumn] = matrix[i][nextColumn];
+			matrix[i][nextColumn] = rememberInt;
 
 		}
-		
 
 		System.out.println(" matrix");
 
