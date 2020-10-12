c99f0dc7556e9602c4316efea9e760ef003087d0
DaryaSHCH <67901229+DaryaSHCH@users.noreply.github.com>
2020-10-12 21:32:29 +0300

    Update TaskSixth.java

--- jonline-module02/src/com/jonline/module02/multidimensional/task06/TaskSixth.java
+++ jonline-module02/src/com/jonline/module02/multidimensional/task06/TaskSixth.java
@@ -14,47 +14,47 @@ import java.util.Scanner;
  */
 public class TaskSixth {
 	public static void main(String[] args) {
-		Scanner scan = new Scanner (System.in);
-		
+		Scanner scan = new Scanner(System.in);
+
 		int matrixSize;
 		int centralPart;
 		centralPart = 0;
-		
+
 		System.out.println("Введите значение для стороны матрицы>>");
-		 
+
 		while (!scan.hasNextInt()) {
 			String str = scan.nextLine();
 			System.out.println("Введите число>>");
 		}
 		matrixSize = scan.nextInt();
-		
-		while ( matrixSize <= 0 && matrixSize % 2 != 0) {
+
+		while (matrixSize <= 0 && matrixSize % 2 != 0) {
 			System.out.println("Введите четное число больше чем 0 >>");
-					matrixSize = scan.nextInt();
+			matrixSize = scan.nextInt();
 		}
-		
-		int [][] matrix = new int [matrixSize][matrixSize];
-		
-		for ( int i = 0; i < matrix.length; i++) {
+
+		int[][] matrix = new int[matrixSize][matrixSize];
+
+		for (int i = 0; i < matrix.length; i++) {
 			for (int j = i; j < matrix[i].length - i; j++) {
 				matrix[i][j] = 1;
-				
+
 			}
 		}
-		for (int i = matrix.length-1; i >= matrix.length/2; i--) {
+		for (int i = matrix.length - 1; i >= matrix.length / 2; i--) {
 			for (int j = centralPart; j < matrix[i].length - centralPart; j++) {
 				matrix[i][j] = 1;
 			}
 			centralPart++;
 		}
-		
-		for ( int [] array : matrix) {
+
+		for (int[] array : matrix) {
 			for (int element : array) {
 				System.out.print(element + "\t");
 			}
 			System.out.println();
 		}
-		
+
 	}
 
 }
