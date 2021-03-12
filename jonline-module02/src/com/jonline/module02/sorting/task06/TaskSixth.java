6220e977507003dc120beb7fd3b5981f3b5f6e92
DaryaSHCH <67901229+DaryaSHCH@users.noreply.github.com>
2021-03-12 19:07:26 +0300

    Update TaskSixth.java

--- jonline-module02/src/com/jonline/module02/sorting/task06/TaskSixth.java
+++ jonline-module02/src/com/jonline/module02/sorting/task06/TaskSixth.java
@@ -13,27 +13,54 @@ import java.util.Scanner;
  */
 public class TaskSixth {
 	public static void main(String[] args) {
-		
+
 		int arraySize;
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("input size for array>> ");
-		
+
 		checkNumber(sc);
-		
+
 		arraySize = sc.nextInt();
 		while (arraySize <= 0) {
 			System.out.println("input integer more then 0 >>");
 			checkNumber(sc);
 			arraySize = sc.nextInt();
 		}
-		int [] array = new int [arraySize];
+		int[] array = new int[arraySize];
 		Random rand = new Random();
 		for (int i = 0; i < array.length; i++) {
-			array[i] = rand.nextInt(10); 
+			array[i] = rand.nextInt(10);
 		}
+
 		System.out.println("initial array>>");
-	}	
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+
+		int d = array.length / 2;
+		while (d > 0) {
+			for (int i = 0; i < array.length - d; i++) {
+				int j = i;
+				while (j >= 0 && array[j] > array[j + d]) {
+					int temp = array[j];
+					array[j] = array[j + d];
+					array[j + d] = temp;
+					j -= d;
+				}
+			}
+			d /= 2;
+		}
+
+		System.out.println();
+		System.out.println("result>>");
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+
+	}
 
 	private static void checkNumber(Scanner sc) {
 		while (!sc.hasNextInt()) {
