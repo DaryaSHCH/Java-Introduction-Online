0c1863158f40aef00f06ae7c08e860f172899667
DaryaSHCH <67901229+DaryaSHCH@users.noreply.github.com>
2021-06-10 03:16:49 +0300

    Create TaskFirst.java

---
+++ jonline-module02/src/com/jonline/module02/decomposition/task01/TaskFirst.java
@@ -0,0 +1,10 @@
+package com.jonline.module02.decomposition.task01;
+/*
+ * Написать метод(методы) для нахождения наибольшего общего делителя и наименьшего общего кратного двух
+ * натуральных чисел:
+ */
+public class TaskFirst {
+public static void main(String[] args) {
+	
+}
+}
