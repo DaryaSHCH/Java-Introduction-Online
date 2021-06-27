41722fdbc6d13144cb7c996634177ae82d408d09
DaryaSHCH <67901229+DaryaSHCH@users.noreply.github.com>
2021-06-27 14:48:56 +0300

    Update TaskFifth.java

--- jonline-module02/src/com/jonline/module02/decomposition/task05/TaskFifth.java
+++ jonline-module02/src/com/jonline/module02/decomposition/task05/TaskFifth.java
@@ -9,71 +9,72 @@ import java.util.Scanner;
 которое меньше максимального элемента массива, но больше всех других элементов).
  */
 public class TaskFifth {
-public static void main(String[] args) {
-	
-	Scanner reader = new Scanner(System.in);
-	
-	int n; 
-	
-	System.out.println("Input array length >>");
-	
-	checkInteger(reader);
-	
-	n = reader.nextInt();
-	
-	while( n <= 0) {
+	public static void main(String[] args) {
+
+		Scanner reader = new Scanner(System.in);
+
+		int n;
+
+		System.out.println("Input array length >>");
+
 		checkInteger(reader);
-		System.out.println("Input integer more then 0 >>");
+
 		n = reader.nextInt();
-	}
-	
-	int[] array = createArray(n);
-	
-	System.out.println("ARRAY");
-	
-	System.out.println(Arrays.toString(array));
-	
-	int subMax = findIntLessThanMax(array);
-	
-	System.out.println("The number is less than the maximum number in the array, but more than the remaining of elements  " +  subMax);
-	
-	
-}
 
-public static int findIntLessThanMax(int[] array) {
-	int max = array[0];
-	
-	for (int i = 0; i < array.length; i++) {
-		if (array[i] > max) {
-			max = array[i];
+		while (n <= 0) {
+			checkInteger(reader);
+			System.out.println("Input integer more then 0 >>");
+			n = reader.nextInt();
+		}
+
+		int[] array = createArray(n);
+
+		System.out.println("ARRAY");
+
+		System.out.println(Arrays.toString(array));
+
+		int subMax = findIntLessThanMax(array);
+
+		System.out.println(
+				"The number is less than the maximum number in the array, but more than the remaining of elements  "
+						+ subMax);
+
+	}
+
+	public static int findIntLessThanMax(int[] array) {
+		int max = array[0];
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > max) {
+				max = array[i];
+			}
+		}
+
+		int subMax = array[0];
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > subMax && array[i] < max) {
+				subMax = array[i];
+			}
+		}
+		return subMax;
+	}
+
+	private static int[] createArray(int n) {
+		int[] array = new int[n];
+
+		Random rand = new Random();
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = rand.nextInt(100);
+		}
+		return array;
+	}
+
+	private static void checkInteger(Scanner reader) {
+		while (!reader.hasNextInt()) {
+			String line = reader.nextLine();
+			System.out.println("Input integer more then 0 >>");
 		}
 	}
-	
-	int subMax = array[0]; 
-	
-	for (int i = 0; i < array.length; i++) {
-		if (array[i] > subMax && array[i] < max) {
-			subMax = array[i];
-		}
-	}
-	return subMax;
-}
-
-private static int[] createArray(int n) {
-	int [] array = new int [n];
-	
-	Random rand = new Random();
-	
-	for (int i = 0; i < array.length; i++) {
-		array[i] = rand.nextInt(100);
-	}
-	return array;
-}
-
-private static void checkInteger(Scanner reader) {
-	while(!reader.hasNextInt()) {
-		String line = reader.nextLine();
-		System.out.println("Input integer more then 0 >>");
-	}
-}
 }
