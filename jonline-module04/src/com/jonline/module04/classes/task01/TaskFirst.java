a183aec80750d5fd3968fb98b61414296f7cab1e
DaryaSHCH <67901229+DaryaSHCH@users.noreply.github.com>
2021-07-31 15:08:57 +0300

    Update TaskFirst.java

--- jonline-module04/src/com/jonline/module04/classes/task01/TaskFirst.java
+++ jonline-module04/src/com/jonline/module04/classes/task01/TaskFirst.java
@@ -1,9 +1,24 @@
 package com.jonline.module04.classes.task01;
+
 /*
  * Создайте класс Test1 двумя переменными. Добавьте метод вывода на экран и методы изменения этих
  * переменных. Добавьте метод, который находит сумму значений этих переменных, и метод, который находит
  * наибольшее значение из этих двух переменных.
  */
 public class TaskFirst {
+	public static void main(String[] args) {
+
+		TestFirst testCurrent = new TestFirst();
+
+		testCurrent.setLength(10);
+		testCurrent.setWidth(6);
+
+		System.out.println("initial variables");
+		testCurrent.printVarFromTest(testCurrent.getLength(), testCurrent.getWidth());
+
+		System.out.println("Sum = " + testCurrent.findSumVar());
+		System.out.println("The greatest variable is " + testCurrent.findGreatestVar());
+
+	}
 
 }
