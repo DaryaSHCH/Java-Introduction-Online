ea0bf1a98313fdac6924e2febf888cee7f9d8f10
DaryaSHCH <67901229+DaryaSHCH@users.noreply.github.com>
2021-08-02 21:33:15 +0300

    Create TaskSecond.java

---
+++ jonline-module04/src/com/jonline/module04/classes/task02/TaskSecond.java
@@ -0,0 +1,11 @@
+package com.jonline.module04.classes.task02;
+/*
+ * Создйте класс Test2 двумя переменными. Добавьте конструктор с входными параметрами. Добавьте
+ * конструктор, инициализирующий члены класса по умолчанию. Добавьте set- и get- методы для полей экземпляра
+ * класса.
+ */
+public class TaskSecond {
+	public static void main(String[] args) {
+
+	}
+}
