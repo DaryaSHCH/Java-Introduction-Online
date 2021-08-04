59e6cdffda91905c4c4e0f41516de9e2247cbde2
DaryaSHCH <67901229+DaryaSHCH@users.noreply.github.com>
2021-08-04 13:08:15 +0300

    Update Student.java

--- jonline-module04/src/com/jonline/module04/classes/task03/Student.java
+++ jonline-module04/src/com/jonline/module04/classes/task03/Student.java
@@ -1,33 +1,51 @@
 package com.jonline.module04.classes.task03;
 
 public class Student {
-	
-	private String surname;
-	private String initials; 
+	private String surnameAndInitials;
 	private int groupNumber;
-	private int [] studentPerformance;
-	
-		public void setSurname(String surname){
-			this.surname = surname; 
+	private int[] studentProgress = new int[5];
+
+	public Student(String surnameAndInitials, int groupNumber, int[] studentProgress) {
+		this.surnameAndInitials = surnameAndInitials;
+		this.groupNumber = groupNumber;
+		this.studentProgress = studentProgress;
+
+	}
+
+	public void setSurnameAndInitials(String surnameAndInitials) {
+		this.surnameAndInitials = surnameAndInitials;
+	}
+
+	public String getSurnameAndInitials() {
+		return this.surnameAndInitials;
+	}
+
+	public void setGroupNumber(int groupNumber) {
+		this.groupNumber = groupNumber;
+	}
+
+	public int getGroupNumber() {
+		return this.groupNumber;
+	}
+
+	public void setStudentProgress(int[] studentProgress) {
+		this.studentProgress = studentProgress;
+	}
+
+	public int[] getStudentProgress() {
+		return this.studentProgress;
+	}
+
+	public static void printBestMarks(Student[] listStudents) {
+		for (Student student : listStudents) {
+			int tempCounter = 0;
+			for (int mark : student.studentProgress) {
+				if (mark < 9) {
+					tempCounter++;
+				}
+			}
+			if (tempCounter == 0)
+				System.out.println(student.surnameAndInitials + "  " + student.groupNumber);
 		}
-		
-		public String getSurname() {
-			return this.surname; 
-		}
-		
-		public void setInitials(String initials) {
-			this.initials = initials; 
-		}
-		
-		public String getInitials() {
-			return this.initials;
-		}
-	
-		public void setGroupNumber(int groupNumber) {
-			this.groupNumber = groupNumber; 
-		}
-		
-		public int getGroupNumber() {
-			return this.groupNumber; 
-		}
+	}
 }
