f5f929c42707b73e453139b665eca05a707e7ab8
DaryaSHCH <67901229+DaryaSHCH@users.noreply.github.com>
2021-08-13 21:21:04 +0300

    Update TaskFourth.java

--- jonline-module04/src/com/jonline/module04/classes/task04/TaskFourth.java
+++ jonline-module04/src/com/jonline/module04/classes/task04/TaskFourth.java
@@ -1,5 +1,7 @@
 package com.jonline.module04.classes.task04;
 
+import java.util.Scanner;
+
 /*
  * Создайте класс Train, содержащий поля: название пункта назначения, номер поезда, время отправления.
 Создайте данные в массив из пяти элементов типа Train, добавьте возможность сортировки элементов массива по
@@ -9,13 +11,14 @@ package com.jonline.module04.classes.task04;
  */
 public class TaskFourth {
 	public static void main(String[] args) {
+		int numberPassengerTrain;
 
 		Train[] trains = new Train[] { new Train("St.Petersburg", 11, 21, 55), new Train("Moscow", 7, 7, 0),
 				new Train("Blagoveschensk", 22, 17, 30), new Train("Sortavala", 15, 13, 17),
 				new Train("Ivanovka", 12, 14, 55) };
 
-		sortByNumber(trains);
-		printTrains(trains);
+		numberPassengerTrain = inputNumberTrain("number of the train available to you(11,7,22,15 or 12) >>");
+		printInfoTrain(numberPassengerTrain, trains);
 	}
 
 	public static void sortByNumber(Train[] trains) {
@@ -37,4 +40,54 @@ public class TaskFourth {
 					+ ", Departure time " + train.getDepartureTime() + "]");
 		}
 	}
+
+	public static Train[] sortByDestination(Train[] trains) {
+		Train temporaryVar;
+		for (int i = 0; i < trains.length; i++) {
+			for (int j = 0; j < trains.length - 1; j++) {
+				if (trains[j].getDestination().compareTo(trains[j + 1].getDestination()) > 0) {
+					temporaryVar = trains[j];
+					trains[j] = trains[j + 1];
+					trains[j + 1] = temporaryVar;
+				} else if (trains[j].getDestination().compareTo(trains[j + 1].getDestination()) == 0) {
+					if (trains[j].getDepartureTime().compareTo(trains[j + 1].getDepartureTime()) > 0) {
+						temporaryVar = trains[j];
+						trains[j] = trains[j + 1];
+						trains[j + 1] = temporaryVar;
+					}
+				}
+			}
+		}
+		return trains;
+	}
+
+	@SuppressWarnings("resource")
+	public static int inputNumberTrain(String message) {
+		Scanner reader;
+		int number;
+
+		reader = new Scanner(System.in);
+
+		do {
+			System.out.println("Input " + message);
+
+			while (!reader.hasNextInt()) {
+				System.out.println("Input number of the train available to you>>");
+				reader.next();
+			}
+			number = reader.nextInt();
+		} while ((number != 11) && (number != 7) && (number != 22) && (number != 15) && (number != 12));
+
+		return number;
+	}
+
+	public static void printInfoTrain(int numberTrain, Train[] trains) {
+		for (int i = 0; i < trains.length; i++) {
+			if (trains[i].getTrainNumber() == numberTrain) {
+				System.out.println("[Destination: " + trains[i].getDestination() + ", №" + trains[i].getTrainNumber()
+						+ ", Departure time " + trains[i].getDepartureTime() + "]");
+			}
+		}
+	}
+
 }
