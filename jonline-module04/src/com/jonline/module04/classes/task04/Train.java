3017ab844c4505acb0b7f48b5f51d3fcc4565798
DaryaSHCH <67901229+DaryaSHCH@users.noreply.github.com>
2021-08-13 19:22:24 +0300

    Update Train.java

--- jonline-module04/src/com/jonline/module04/classes/task04/Train.java
+++ jonline-module04/src/com/jonline/module04/classes/task04/Train.java
@@ -1,6 +1,46 @@
 package com.jonline.module04.classes.task04;
 
+import java.time.LocalTime;
+
 public class Train {
-   private int trainNumber; 
-   private String destination; 
+	private int trainNumber;
+	private String destination;
+	private LocalTime departureTime;
+
+	public Train(String destination, int trainNumber) {
+		this.destination = destination;
+		this.trainNumber = trainNumber;
+		this.departureTime = LocalTime.of(1, 30);
+	}
+
+	public Train(String destination, int trainNumber, int hour, int min) {
+		this.destination = destination;
+		this.trainNumber = trainNumber;
+		this.departureTime = LocalTime.of(hour, min);
+	}
+
+	public void setTrainNumber(int trainNumber) {
+		this.trainNumber = trainNumber;
+	}
+
+	public int getTrainNumber() {
+		return this.trainNumber;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public String getDestination() {
+		return this.destination;
+	}
+
+	public void setDepartureTime(int hour, int min) {
+		this.departureTime = LocalTime.of(hour, min);
+	}
+
+	public LocalTime getDepartureTime() {
+		return this.departureTime;
+	}
+
 }
