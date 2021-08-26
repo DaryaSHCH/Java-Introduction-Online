1f10392eaaddd40f0936c0cac97bed7f53f86d73
DaryaSHCH <67901229+DaryaSHCH@users.noreply.github.com>
2021-08-26 16:42:07 +0300

    Create TaskSeventh.java

---
+++ jonline-module04/src/com/jonline/module04/classes/task07/TaskSeventh.java
@@ -0,0 +1,10 @@
+package com.jonline.module04.classes.task07;
+/*
+ * Описать класс, представляющий треугольник. Предусмотреть методы для создания объектов, вычисления
+площади, периметра и точки пересечения медиан.
+ */
+public class TaskSeventh {
+	public static void main(String[] args) {
+		
+	}
+}
