c26b126fcaccb27a489b09078030f9e66792d5de
DaryaSHCH <67901229+DaryaSHCH@users.noreply.github.com>
2021-09-01 17:29:49 +0300

    Update CustomerData.java

--- jonline-module04/src/com/jonline/module04/classes/task08/CustomerData.java
+++ jonline-module04/src/com/jonline/module04/classes/task08/CustomerData.java
@@ -1,5 +1,26 @@
 package com.jonline.module04.classes.task08;
 
 public class CustomerData {
+	
+	private Customer[] customerData = new Customer[2];
+	private int actualLength = 0;
 
+	public void addCustomer(final Customer newCustomer) {
+		checkLengthAndExtend();
+
+		customerData[actualLength] = newCustomer;
+		actualLength++;
+	}
+
+	private void checkLengthAndExtend() {
+		if (actualLength >= customerData.length) {
+			final Customer[] extendedCustomerData = new Customer[customerData.length * 2];
+
+			for (int i = 0; i < customerData.length; i++) {
+				extendedCustomerData[i] = customerData[i];
+			}
+
+			customerData = extendedCustomerData;
+		}
+	}
 }
